a5ad8ea85be16272d0ae28d711c720e5e425288c
Bala <110883394+Bala0035@users.noreply.github.com>
2022-11-05 11:57:20 +0530

    Update login.dart

--- login.dart
+++ login.dart
@@ -1,12 +1,7 @@
 //Log in Page
-import 'package:demo/home.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_login/flutter_login.dart';
 
-const users = const {
-  'dribbble@gmail.com': '12345',
-  'hunter@gmail.com': 'hunter',
-};
+import 'back.dart';
 
 class login extends StatefulWidget {
   const login({super.key});
@@ -16,113 +11,110 @@ class login extends StatefulWidget {
 }
 
 class _loginState extends State<login> {
-  Duration get loginTime => Duration(milliseconds: 2250);
-
-  Future<String?> _authUser(LoginData data) {
-    debugPrint('Name: ${data.name}, Password: ${data.password}');
-    return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(data.name)) {
-        return 'User not exists';
-      }
-      if (users[data.name] != data.password) {
-        return 'Password does not match';
-      }
-      return null;
-    });
-  }
-
-  Future<String?> _signupUser(SignupData data) {
-    debugPrint('Signup Name: ${data.name}, Password: ${data.password}');
-    return Future.delayed(loginTime).then((_) {
-      return null;
-    });
-  }
-
-  Future<String> _recoverPassword(String name) {
-    debugPrint('Name: $name');
-    return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(name)) {
-        return 'User not exists';
-      }
-      return "null";
-    });
-  }
+    final userName= TextEditingController();
+    final password = TextEditingController();
+    var key = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    final _textEditingNameController = TextEditingController();
-    final _textEditingPasswordController = TextEditingController();
     var loginscreen = false;
     return Scaffold(
       body: Center(
-        child: Container(
-          height: 380,
-          width: 310,
-          decoration: BoxDecoration(
-              color: Colors.blue, borderRadius: BorderRadius.circular(30)),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Enter Name",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 1),
-                margin: EdgeInsets.symmetric(vertical: 10),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15)),
-                height: 50,
-                width: 260,
-                child: TextFormField(
-                  controller: _textEditingNameController,
-                  decoration: InputDecoration(hintText: "Full Name"),
+        child: Form(         
+      key:  key,
+          child: Container(
+            height: 380,
+            width: 310,
+            decoration: BoxDecoration(
+                color: Colors.blue, borderRadius: BorderRadius.circular(30)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Enter Name",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
                 ),
-              ),
-              Text(
-                "Enter Password",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 1),
-                margin: EdgeInsets.symmetric(vertical: 10),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15)),
-                height: 50,
-                width: 260,
-                child: TextFormField(
-                  controller: _textEditingPasswordController,
-                  decoration: InputDecoration(hintText: "Password"),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 1),
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15)),
+                  height: 50,
+                  width: 260,
+                  child: TextFormField(
+                    validator: (name) {
+                      if(name == "bala"){
+                        return null;
+                      }
+                      else{
+                        return "username is wrong";
+                      }
+                    },
+                    controller:userName,
+                    decoration: InputDecoration(hintText: "Full Name"),
+                  ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 20),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(15))),
-                height: 40,
-                width: 110,
-                child: Center(
-                  child: InkWell(
-                    onTap: () {},
-                    child: Text(
-                      "Log In",
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
+                const Text(
+                  "Enter Password",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 1),
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15)),
+                  height: 50,
+                  width: 260,
+                  child: TextFormField(
+                    validator: (pass) {
+                      if(pass == "123"){
+                        return null;
+                      }
+                      else{
+                        return "wrong passward";
+                      }
+                    },
+                    controller: password,
+                    decoration: const InputDecoration(hintText: "Password"),
+                  ),
+                ),
+                InkWell(
+                   onTap: () {
+                   if(key.currentState!.validate()){
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context) =>My()));
+
+                   }
+                   else{
+                    print("passward &username is wrong");
+                   }
+                      },
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 20),
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                    height: 40,
+                    width: 110,
+                    child: Center(
+                      child: Text(
+                        "Log In",
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
